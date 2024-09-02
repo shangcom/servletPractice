56f7ac284b218e1f1f3d0d0efb8c8a074e59e60c
shangcom <dndbs91@gmail.com>
2024-09-02 13:41:50 +0900

    4

---
+++ src/main/java/hello/servletPractice/web/servlet/MemberListServlet.java
@@ -0,0 +1,70 @@
+package hello.servletPractice.web.servlet;
+
+import hello.servletPractice.domain.member.Member;
+import hello.servletPractice.domain.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "memberListServlet", urlPatterns = "/servlet/members/list")
+public class MemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll();
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter w = response.getWriter();
+
+        w.write("<html>");
+        w.write("<head>");
+        w.write("    <meta charset=\"UTF-8\">");
+        w.write("    <title>Title</title>");
+        w.write("</head>");
+        w.write("<body>");
+        w.write("<a href=\"/index.html\">메인</a>");
+        w.write("<table>");
+        w.write("    <thead>");
+        w.write("    <th>id</th>");
+        w.write("    <th>username</th>");
+        w.write("    <th>age</th>");
+        w.write("    </thead>");
+        w.write("    <tbody>");
+
+/*
+        // 테스트용
+        w.write("    <tr>");
+        w.write("        <td>1</td>");
+        w.write("        <td>userA</td>");
+
+
+        w.write("        <td>10</td>");
+        w.write("    </tr>");
+*/
+
+        for (Member member : members) {
+            w.write("    <tr>");
+            w.write("        <td>" + member.getId() + "</td>");
+            w.write("        <td>" + member.getUsername() + "</td>");
+            w.write("        <td>" + member.getAge() + "</td>");
+            w.write("    </tr>");
+        }
+
+        w.write("    </tbody>");
+        w.write("</table>");
+        w.write("</body>");
+        w.write("</html>");
+
+    }
+}
